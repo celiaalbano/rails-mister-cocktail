f0a93a87fa69ae6aa0833cd37a5a08332cc95835
celia albano <albano.celia@gmail.com>
2016-11-10 17:49:25 +0100

    Mister cocktail la fête est trop folle

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,14 @@
+require 'json'
+require 'open-uri'
+
+url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+result = JSON.parse(open(url).read)
+
+result["drinks"].each do |a|
+  ingredient = Ingredient.create(name: a["strIngredient1"])
+end
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
